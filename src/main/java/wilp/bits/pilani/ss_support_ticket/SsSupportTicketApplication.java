960e12bac3b755d5b305989542850057e9ef8c9b
Midhun <2023tm93708@wilp.bits-pilani.ac.in>
2024-11-20 09:29:36 +0530

    back_end_code

---
+++ src/main/java/wilp/bits/pilani/ss_support_ticket/SsSupportTicketApplication.java
@@ -0,0 +1,13 @@
+package wilp.bits.pilani.ss_support_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsSupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsSupportTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wilp/bits/pilani/ss_support_ticket/SsSupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package wilp.bits.pilani.ss_support_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsSupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
